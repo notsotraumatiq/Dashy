275ec20c38ab1f4926138ee0dbdc386e76690b92
notsotraumatiq <atiqpatel95@gmail.com>
2023-08-09 17:01:33 -0400

    spin my head round right round

---
+++ dashy-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex bg-black">
+      <div className="flex-grow-0.3 bg-gray-800"></div>
+      <div className="flex-grow-0.7"></div>
+    </div>
+  );
+}
+
+export default App;
