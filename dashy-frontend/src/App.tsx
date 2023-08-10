e7af80569af897e56e86753c54b0972451b98dac
notsotraumatiq <atiqpatel95@gmail.com>
2023-08-09 21:04:27 -0400

    changes

--- dashy-frontend/src/App.tsx
+++ dashy-frontend/src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="flex w-screen h-screen justify-between">
-      <div className=" bg-slate-600 pr-44 px-8">
-        <div className="logo mt-6 ">
+      <div className=" bg-slate-600 w-64">
+        <div className="logo mt-6">
           <div className="rounded-md p-5 m-4 bg-blue-700 w-3"></div>
         </div>
         <div className="mx-5 px-4 py-8 bg-gray-700 rounded-md  border-b-4"></div>
@@ -14,45 +14,88 @@ function App() {
 
         <div>
           <div className="mx-5">
-            <div className="flex gap-4 m-6">
+            <div className="flex gap-4 m-6 items-center ">
               <div className="logo p-4 bg-black rounded-full"></div>
               <h3>Something here</h3>
             </div>
-            <div className="flex gap-4 m-6">
+            <div className="flex gap-4 m-6 items-center">
               <div className="logo p-4 bg-black rounded-full"></div>
               <h3>Something here</h3>
             </div>
-            <div className="flex gap-4 m-6">
+            <div className="flex gap-4 m-6 items-center">
               <div className="logo p-4 bg-black rounded-full"></div>
               <h3>Something here</h3>
             </div>
-            <div className="flex gap-4 m-6">
+            <div className="flex gap-4 m-6 items-center">
               <div className="logo p-4 bg-black rounded-full"></div>
               <h3>Something here</h3>
             </div>
-            <div className="flex gap-4 m-6">
+            <div className="flex gap-4 m-6 items-center">
               <div className="logo p-4 bg-black rounded-full"></div>
               <h3>Something here</h3>
             </div>
-            <div className="flex gap-4 m-6">
+            <div className="flex gap-4 m-6 items-center">
               <div className="logo p-4 bg-black rounded-full"></div>
               <h3>Something here</h3>
             </div>
-
-            <div className="bottom-16">
-              <div className="bg-gray-800  h-[1px] my-12"></div>
-              <div>contact us</div>
-            </div>
+          </div>
+          <div className="bottom-16">
+            <div className="bg-gray-800  h-[1px] my-12"></div>
+            <div>contact us</div>
           </div>
         </div>
       </div>
       <div className="w-70">
-        <div className="flex mt-6 mr-6 gap-8 justify-start">
-          <button className="rounded-full bg-red-700 p-4 flex-shrink "></button>
+        <div className="">
+          <div className="flex mt-6 mr-6 gap-8 justify-end mb-20">
+            {/* couldnt separate out the 1 and the others */}
+            <button className="rounded-full bg-red-700 p-4 flex-shrink "></button>
 
-          <button className="rounded-full bg-slate-900 p-4"></button>
-          <button className="rounded-full bg-slate-900 p-4"></button>
-          <button className="rounded-full bg-slate-900 p-4"></button>
+            <button className="rounded-full bg-slate-900 p-4"></button>
+            <button className="rounded-full bg-slate-900 p-4"></button>
+            <button className="rounded-full bg-slate-900 p-4"></button>
+          </div>
+          <div className="card flex gap-5 mx-4 mt-32 justify-start w-max">
+            <div className="card flex-1 p-12 bg-lime-500 rounded-xl ">
+              <h3>Budget</h3>
+              {/* LOGO Doesnt work */}
+              <div className="logo bg-black rounded-full p-4"></div>
+
+              <h2 className="font-bold text-2xl">$24</h2>
+              <p className="text-base">
+                <strong>12% |</strong> since last month
+              </p>
+            </div>
+            <div className="card flex-1 p-12 bg-lime-500 rounded-xl ">
+              <h3>Budget</h3>
+              <div className="logo bg-black rounded-full p-4 top-0 right-0 "></div>
+              <h2 className="font-bold text-2xl">$24</h2>
+              <p className="text-base">
+                <strong>12% |</strong> since last month
+              </p>
+            </div>
+            <div className="card flex-1 p-12 bg-lime-500 rounded-xl ">
+              <h3>Budget</h3>
+              <div className="logo bg-black rounded-full p-4 top-2 right-4 "></div>
+              <h2 className="font-bold text-2xl">$24</h2>
+              <p className="text-base">
+                <strong>12% |</strong> since last month
+              </p>
+            </div>
+            <div className="card flex-1 p-12 bg-lime-500 rounded-xl ">
+              <h3>Budget</h3>
+              <div className="logo bg-black rounded-full p-4 top-2 right-4 "></div>
+              <h2 className="font-bold text-2xl">$24</h2>
+              <p className="text-base">
+                <strong>12% |</strong> since last month
+              </p>
+            </div>
+          </div>
+          {/* couldnt do space betweem */}
+          <div className="flex w-max mt-7 gap-5 h-20 ">
+            <div className="flex-grow pt-12 pb-48 px-14 bg-red-600 rounded-xl"></div>
+            <div className="flex-grow p-12 bg-red-600 rounded-xl"></div>
+          </div>
         </div>
       </div>
     </div>
